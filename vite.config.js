0f34f10552bb4a05eedb616dd36c9055382114fd
PolinaShchulepova <polinkash28@gmail.com>
2025-02-09 23:11:56 +0200

    Added allowedHosts to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  allowedHosts: ["costmanager-ynh1.onrender.com"],
+  server :{
+    host: true,
+  allowedHosts: ["costmanager-ynh1.onrender.com"]
+},
 })
